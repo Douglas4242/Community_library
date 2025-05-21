cdf5f84d6557e1e2d6c057750601dfc2e6e0794a
Douglas Moraes <douglasgm89@live.com>
2025-05-21 20:36:54 -0300

    Feat: bcrypt and nodemon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,4 +1,5 @@
 import userRepository from "../repositories/user.repositories.js";
+import bcrypt from 'bcrypt'
 
 async function createUserService(newUser){
     const foundUserEmail = await userRepository.findUserByEmailRepository(newUser.email)
@@ -11,8 +12,14 @@ async function createUserService(newUser){
         throw new Error("Username already in use")
     }
 
-    const user = await userRepository.createUserRepository(newUser)
+    const passHash = await bcrypt.hash(newUser.password, 10)
+    
+    const user = await userRepository.createUserRepository({...newUser, password: passHash})
+    if(!user) {
+        throw new Error("Error creating User")
+    }
     return user
 }
+
 
 export default {createUserService}

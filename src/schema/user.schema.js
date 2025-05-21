ba031961e4c03b46a0b904d5135ed1bcd5d1798b
Douglas Moraes <douglasgm89@live.com>
2025-05-21 20:59:48 -0300

    Feat: user schema

---
+++ src/schema/user.schema.js
@@ -0,0 +1,10 @@
+import {z} from 'zod'
+
+const userSchema = z.object({
+    username: z.string().min(3, 'Username is required to be longer than 3 characters'),
+    email: z.string.email('Invalid email'),
+    password: z.string.min(6,'Password must be at least 6 characters'),
+    avatar: z.string.url('Invalid URL').optional()
+})
+
+export { userSchema }
